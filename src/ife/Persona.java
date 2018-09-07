b1236b997e083250e435c3114bc401c49a7de844
frank <frank@192.168.100.9>
2018-09-07 12:29:58 -0500

    Aqui comenzo mi repositorio

---
+++ src/ife/Persona.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ife;
+
+/**
+ *
+ * @author frank
+ */
+public class Persona {
+    
+    int edad;
+    String nombre;
+    String apellidoP;
+    String apellidoM;
+    boolean genero;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public boolean isGenero() {
+        return genero;
+    }
+
+    public void setGenero(boolean genero) {
+        this.genero = genero;
+    }
+   
+    
+    
+    
+}
